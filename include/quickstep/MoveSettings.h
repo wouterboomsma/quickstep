70ef6099d50cc7d65666f7aea51ed26f20815efa
Wouter Boomsma <wb@di.ku.dk>
2016-06-01 11:49:09 +0200

    Added missing files.

---
+++ include/quickstep/MoveSettings.h
@@ -0,0 +1,16 @@
+#ifndef PHAISTOS_MOVE_SETTINGS_H
+#define PHAISTOS_MOVE_SETTINGS_H
+
+namespace quickstep {
+
+// Base class for Settings in energy terms
+class MoveSettings {
+public:
+    MoveSettings() { }
+
+    virtual ~MoveSettings() { }
+};
+
+}
+
+#endif //PHAISTOS_MOVE_SETTINGS_H
